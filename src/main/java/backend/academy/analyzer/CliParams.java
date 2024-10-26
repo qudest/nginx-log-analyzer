1b4aca3f57619a62fe3a08a58f35b3ed9470cf66
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-26 12:46:32 +0300

    feat: jcommander cli params

---
+++ src/main/java/backend/academy/analyzer/CliParams.java
@@ -0,0 +1,49 @@
+package backend.academy.analyzer;
+
+import com.beust.jcommander.Parameter;
+import lombok.Getter;
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+public class CliParams {
+
+    @Parameter(
+        names = "--path",
+        description = "Path to one or more NGINX log files as a local template or URL",
+        variableArity = true,
+        required = true
+    )
+    private List<String> path;
+
+    @Parameter(
+        names = "--from",
+        description = "Analysis start time in ISO8601 format (optional)"
+    )
+    private Instant from;
+
+    @Parameter(
+        names = "--to",
+        description = "Analysis end time in ISO8601 format (optional)"
+    )
+    private Instant to;
+
+    @Parameter(
+        names = "--format",
+        description = "Output format (optional)"
+    )
+    private OutputFormat format;
+
+    @Parameter(
+        names = "--filter-field",
+        description = "Filter field (optional)"
+    )
+    private String filterField;
+
+    @Parameter(
+        names = "--filter-value",
+        description = "Filter value (optional)"
+    )
+    private String filterValue;
+
+}
