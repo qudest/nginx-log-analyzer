a776a54b3932bce7aac29822443deaa1460d9634
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-30 11:49:28 +0300

    test: тестировать ArgsParser

---
+++ src/test/java/backend/academy/analyzer/parser/ArgsParserTest.java
@@ -0,0 +1,97 @@
+package backend.academy.analyzer.parser;
+
+import backend.academy.analyzer.FilterField;
+import backend.academy.analyzer.OutputFormat;
+import backend.academy.analyzer.Params;
+import com.beust.jcommander.ParameterException;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgsParserTest {
+
+    private final ParamsParser paramsParser = new ArgsParser();
+
+    @Test
+    void parse() {
+        Params params = Params.builder()
+            .path(List.of(
+                "logs/**/2024-08-31.txt",
+                "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs"
+            ))
+            .from(LocalDateTime.of(2024, 10, 25, 0, 0, 0).toInstant(ZoneOffset.UTC))
+            .to(LocalDateTime.of(2024, 10, 28, 0, 0, 0).toInstant(ZoneOffset.UTC))
+            .format(OutputFormat.ADOC)
+            .filterField(FilterField.AGENT)
+            .filterValue("Mozilla")
+            .build();
+        String[] args = {
+            "--path",
+            "logs/**/2024-08-31.txt",
+            "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs",
+            "--from",
+            "2024-10-25",
+            "--to",
+            "2024-10-28",
+            "--format",
+            "adoc",
+            "--filter-field",
+            "agent",
+            "--filter-value",
+            "Mozilla"
+        };
+        Params parsed = paramsParser.parse(args);
+        assertEquals(params, parsed);
+    }
+
+    @Test
+    void parseWithoutPath() {
+        String[] args = {
+            "--from",
+            "2024-10-25",
+            "--to",
+            "2024-10-28",
+            "--format",
+            "adoc",
+            "--filter-field",
+            "agent",
+            "--filter-value",
+            "Mozilla"
+        };
+        assertThrows(ParameterException.class, () -> paramsParser.parse(args));
+    }
+
+    @Test
+    void parseWithWrongTimestamp() {
+        String[] args = {
+            "--path",
+            "logs/**/2024-08-31.txt",
+            "--from",
+            "2024-10-28", // после to
+            "--to",
+            "2024-10-25",
+        };
+        assertThrows(ParameterException.class, () -> paramsParser.parse(args));
+    }
+
+    @Test
+    void parseWithWrongEnumType() {
+        String[] args1 = {
+            "--path",
+            "logs/**/2024-08-31.txt",
+            "--format",
+            "TEST"
+        };
+        assertThrows(ParameterException.class, () -> paramsParser.parse(args1));
+
+        String[] args2 = {
+            "--path",
+            "logs/**/2024-08-31.txt",
+            "--filter-field",
+            "TEST"
+        };
+        assertThrows(ParameterException.class, () -> paramsParser.parse(args2));
+    }
+}
