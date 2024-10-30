f1186b200a7a7cb180b641dc6dad21adfc0799f4
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-10-30 10:01:42 +0300

    feat: реализовать парсер аргументов

---
+++ src/main/java/backend/academy/analyzer/parser/ArgsParser.java
@@ -0,0 +1,25 @@
+package backend.academy.analyzer.parser;
+
+import backend.academy.analyzer.Params;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+public class ArgsParser implements ParamsParser {
+
+    @Override
+    public Params parse(String[] args) {
+        Params params = new Params();
+        JCommander jc = JCommander.newBuilder()
+                .addObject(params)
+                .build();
+        try {
+            jc.parse(args);
+            params.validate();
+        } catch (ParameterException e) {
+            jc.usage();
+            throw e;
+        }
+        return params;
+    }
+
+}
