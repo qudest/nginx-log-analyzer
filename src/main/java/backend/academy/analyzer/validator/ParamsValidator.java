6eac2b329209ce88788333cdee2f2702978c859d
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-07 14:31:19 +0300

    feat: валидация filter value

--- src/main/java/backend/academy/analyzer/validator/ParamsValidator.java
+++ src/main/java/backend/academy/analyzer/validator/ParamsValidator.java
@@ -8,6 +8,7 @@ public final class ParamsValidator {
 
     public static void validate(Params params) {
         validateTimeRange(params);
+        validateFilterValue(params);
     }
 
     private static void validateTimeRange(Params params) {
@@ -15,6 +16,12 @@ public final class ParamsValidator {
         Instant to = params.to();
         if (from != null && to != null && from.isAfter(to)) {
             throw new ParameterException("The start time cannot be later than the end time");
+        }
+    }
+
+    private static void validateFilterValue(Params params) {
+        if (params.filterField() == null && params.filterValue() != null) {
+            throw new ParameterException("If you use a filter value, the filter field is required");
         }
     }
 
