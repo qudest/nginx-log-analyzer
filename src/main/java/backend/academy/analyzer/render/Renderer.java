2be3dea76294e7fe604bde3bfec5172887293130
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-09 02:49:44 +0300

    refactor: сделать абстрактным классом

--- src/main/java/backend/academy/analyzer/render/Renderer.java
+++ src/main/java/backend/academy/analyzer/render/Renderer.java
@@ -1,9 +1,28 @@
 package backend.academy.analyzer.render;
 
 import backend.academy.analyzer.log.LogReport;
+import backend.academy.analyzer.stats.Metric;
+import backend.academy.analyzer.stats.Table;
+import java.util.List;
 
-public interface Renderer {
+public abstract class Renderer {
 
-    String render(LogReport logReport);
+    public String render(LogReport logReport) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Table table : logReport.tables()) {
+            sb.append(renderHeader(table.name()));
+            sb.append(renderTableHeaders(table.headers()));
+            sb.append(renderTableValues(table.values()));
+        }
+
+        return sb.toString();
+    }
+
+    abstract String renderHeader(String header);
+
+    abstract String renderTableHeaders(List<String> headers);
+
+    abstract String renderTableValues(List<Metric> values);
 
 }
