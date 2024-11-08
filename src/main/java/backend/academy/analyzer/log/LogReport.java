6f3ee99820ec0728bc279eb1172d28abb2e0a145
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-09 01:48:43 +0300

    refactor: убрать лишние поля

--- src/main/java/backend/academy/analyzer/log/LogReport.java
+++ src/main/java/backend/academy/analyzer/log/LogReport.java
@@ -1,6 +1,5 @@
 package backend.academy.analyzer.log;
 
-import backend.academy.analyzer.stats.Metric;
 import backend.academy.analyzer.stats.Table;
 import java.util.List;
 import lombok.Builder;
@@ -8,8 +7,6 @@ import lombok.Builder;
 @Builder
 public record LogReport(
 
-    List<Metric> headers,
-    List<Metric> metrics,
     List<Table> tables
 
 ) {
