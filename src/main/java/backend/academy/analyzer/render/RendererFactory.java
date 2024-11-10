b216fa0cd9866b14572e3d32646b9795c1ca01c1
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-10 22:39:02 +0300

    refactor: сделать final

--- src/main/java/backend/academy/analyzer/render/RendererFactory.java
+++ src/main/java/backend/academy/analyzer/render/RendererFactory.java
@@ -2,13 +2,16 @@ package backend.academy.analyzer.render;
 
 import backend.academy.analyzer.params.OutputFormat;
 
-public class RendererFactory {
+public final class RendererFactory {
 
     public static Renderer getRenderer(OutputFormat outputFormat) {
         if (outputFormat == OutputFormat.ADOC) {
             return new AdocRenderer();
         }
         return new MarkdownRenderer();
+    }
+
+    private RendererFactory() {
     }
 
 }
