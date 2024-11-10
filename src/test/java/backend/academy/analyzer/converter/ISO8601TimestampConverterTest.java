845273a0b640d801a6587a909f4ab06754175603
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-10 20:06:36 +0300

    test: сделать доп тесты

--- src/test/java/backend/academy/analyzer/converter/ISO8601TimestampConverterTest.java
+++ src/test/java/backend/academy/analyzer/converter/ISO8601TimestampConverterTest.java
@@ -1,10 +1,12 @@
 package backend.academy.analyzer.converter;
 
+import com.beust.jcommander.ParameterException;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ISO8601TimestampConverterTest {
 
@@ -42,6 +44,30 @@ class ISO8601TimestampConverterTest {
         String ts = "20241025T144239Z";
         assertEquals(
             Instant.parse("2024-10-25T14:42:39Z"),
+            converter.convert(ts)
+        );
+    }
+
+    @Test
+    void convertNull() {
+        assertThrows(ParameterException.class, () -> converter.convert(null));
+    }
+
+    @Test
+    void convertInvalid() {
+        String ts1 = "2024-10-25T14:42:39";
+        String ts2 = "2024-10-25 14:42:39";
+        String ts3 = "2024-10-25T14:42:39+19:00"; // Zone offset not in valid range: -18:00 to +18:00
+        assertThrows(ParameterException.class, () -> converter.convert(ts1));
+        assertThrows(ParameterException.class, () -> converter.convert(ts2));
+        assertThrows(ParameterException.class, () -> converter.convert(ts3));
+    }
+
+    @Test
+    void convertWithMillis() {
+        String ts = "2024-10-25T14:42:39.123Z";
+        assertEquals(
+            Instant.parse("2024-10-25T14:42:39.123Z"),
             converter.convert(ts)
         );
     }
