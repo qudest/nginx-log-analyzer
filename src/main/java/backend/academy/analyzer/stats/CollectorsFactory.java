e9d17b2a6b2ae08a8f1234ea68230ade3c1b1a0a
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-14 18:18:24 +0300

    feat: сделать метод получения коллектора для LogReport

--- src/main/java/backend/academy/analyzer/stats/CollectorsFactory.java
+++ src/main/java/backend/academy/analyzer/stats/CollectorsFactory.java
@@ -1,11 +1,14 @@
 package backend.academy.analyzer.stats;
 
 import backend.academy.analyzer.log.LogRecord;
+import backend.academy.analyzer.log.LogReport;
 import backend.academy.analyzer.utils.PercentileCalculator;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("MultipleStringLiterals")
 public final class CollectorsFactory {
@@ -24,6 +27,7 @@ public final class CollectorsFactory {
                 values -> new Metric("95p размера ответа",
                     String.valueOf(PercentileCalculator.calculate(PERCENTILE, values)))),
 
+            // доп статистика
             Collectors.collectingAndThen(Collectors.mapping(LogRecord::remoteAddr, Collectors.toSet()),
                 uniqueClients -> new Metric("Количество уникальных клиентов", String.valueOf(uniqueClients.size()))));
     }
@@ -46,12 +50,28 @@ public final class CollectorsFactory {
                         .limit(LIMIT)
                         .toList())),
 
+            // доп статистика
             Collectors.collectingAndThen(Collectors.groupingBy(LogRecord::remoteAddr, Collectors.counting()),
                 map -> new Table("Частые клиенты", List.of("IP", "Количество"),
                     map.entrySet().stream().map(e -> new Metric(e.getKey(), String.valueOf(e.getValue())))
                         .sorted(comparator)
                         .limit(LIMIT)
                         .toList())));
+    }
+
+    public static Collector<LogRecord, ?, LogReport> getLogReportCollector(List<Metric> paramsMetrics) {
+        return Collectors.teeing(
+            MultiCollector.get(getMetricCollectors()),
+            MultiCollector.get(getTableCollectors()),
+            (metrics, tables) -> {
+                List<Table> allTables = new ArrayList<>();
+                allTables.add(new Table("Общая информация", List.of("Метрика", "Значение"),
+                    Stream.concat(paramsMetrics.stream(), metrics.stream()).toList()
+                ));
+                allTables.addAll(tables);
+                return LogReport.builder().tables(allTables).build();
+            }
+        );
     }
 
     private CollectorsFactory() {
