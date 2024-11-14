63d6acac67566843d2030e1fc9fb72fe493b29ad
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-14 18:26:28 +0300

    refactor: заменить Entry на PathSplit

--- src/main/java/backend/academy/analyzer/reader/FileReader.java
+++ src/main/java/backend/academy/analyzer/reader/FileReader.java
@@ -1,22 +1,22 @@
 package backend.academy.analyzer.reader;
 
+import backend.academy.analyzer.utils.PathSplit;
 import backend.academy.analyzer.utils.PathUtils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public class FileReader implements Reader {
 
     @Override
     public Stream<String> read(String path) throws IOException {
-        Map.Entry<String, String> entry = PathUtils.split(path);
-        List<String> files = PathUtils.match(entry.getValue(), entry.getKey());
+        PathSplit split = PathUtils.split(path);
+        List<String> paths = PathUtils.match(split);
         Stream<String> stream = Stream.empty();
-        for (String file : files) {
-            stream = Stream.concat(stream, Files.lines(Path.of(file)));
+        for (String filePath : paths) {
+            stream = Stream.concat(stream, Files.lines(Path.of(filePath)));
         }
         return stream;
     }
