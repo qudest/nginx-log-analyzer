ebdc8f4c208aa7c39667eb370c2659a8d5e72ec2
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-14 19:20:10 +0300

    refactor

--- src/main/java/backend/academy/analyzer/Analyzer.java
+++ src/main/java/backend/academy/analyzer/Analyzer.java
@@ -5,20 +5,16 @@ import backend.academy.analyzer.log.LogReport;
 import backend.academy.analyzer.params.Params;
 import backend.academy.analyzer.parser.LogParser;
 import backend.academy.analyzer.parser.ParamsParser;
-import backend.academy.analyzer.reader.Reader;
-import backend.academy.analyzer.reader.ReaderFactory;
 import backend.academy.analyzer.render.Renderer;
 import backend.academy.analyzer.render.RendererFactory;
 import backend.academy.analyzer.stats.CollectorsFactory;
 import backend.academy.analyzer.stats.Metric;
-import backend.academy.analyzer.stats.StatisticsCollector;
-import backend.academy.analyzer.stats.Table;
+import backend.academy.analyzer.utils.ReaderUtils;
 import backend.academy.analyzer.writer.Writer;
 import com.beust.jcommander.ParameterException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Analyzer {
@@ -49,52 +45,24 @@ public class Analyzer {
         }
         filterChain.params(params);
 
-        List<String> paths = params.path();
-        Stream<String> stream = Stream.empty();
-        for (String path : paths) {
-            Reader reader = ReaderFactory.getReader(path);
-            try {
-                stream = Stream.concat(stream, reader.read(path));
-            } catch (Exception e) {
-                errorOutput.println(path + ": " + e.getMessage());
-            }
-        }
-
-        List<Metric> headers = List.of(
-            new Metric("Файл(-ы)", String.join(", ", paths)),
-            new Metric("Начальная дата", params.from() == null ? "-" : params.from().toString()),
-            new Metric("Конечная дата", params.to() == null ? "-" : params.to().toString())
-        );
-
-        LogReport logReport = stream
-            .map(logParser::parse)
-            .filter(filterChain::filter)
-            .collect(
-                Collectors.teeing(
-                    StatisticsCollector.get(CollectorsFactory.getMetricCollectors()),
-                    StatisticsCollector.get(CollectorsFactory.getTableCollectors()),
-                    (metrics, tables) -> {
-                        List<Table> allTables = new ArrayList<>();
-                        allTables.add(new Table("Общая информация", List.of("Метрика", "Значение"),
-                            Stream.concat(headers.stream(), metrics.stream()).toList()
-                        ));
-                        allTables.addAll(tables);
-
-                        return LogReport.builder()
-                            .tables(allTables)
-                            .build();
-                    }
-                )
-            );
-
+        Stream<String> lines = ReaderUtils.readPaths(params.paths());
+        LogReport logReport = getLogReport(lines, params.getInfoMetrics());
         Renderer renderer = RendererFactory.getRenderer(params.format());
-        String render = renderer.render(logReport);
 
         try {
-            writer.write(render, params.format());
+            writer.write(renderer.render(logReport), params.format());
         } catch (Exception e) {
             errorOutput.println(e.getMessage());
         }
+    }
+
+    private LogReport getLogReport(Stream<String> lines, List<Metric> paramsInfoMetrics) {
+        return lines
+            .map(logParser::parse)
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .filter(filterChain::filter)
+            .collect(CollectorsFactory.getLogReportCollector(paramsInfoMetrics));
     }
 
 }
