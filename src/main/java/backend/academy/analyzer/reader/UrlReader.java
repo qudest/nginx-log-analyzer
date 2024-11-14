31b543aa4a2449d8fefe130f437c4707f67add47
Arsenii Nagornyi <nagornyi.arsenii@mail.ru>
2024-11-14 19:44:50 +0300

    fix: пофиксить DM_DEFAULT_ENCODING

--- src/main/java/backend/academy/analyzer/reader/UrlReader.java
+++ src/main/java/backend/academy/analyzer/reader/UrlReader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -13,7 +14,8 @@ public class UrlReader implements Reader {
     @Override
     public Stream<String> read(String path) throws IOException {
         URL url = URI.create(path).toURL();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+        try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))) {
             List<String> lines = reader.lines().toList();
             return lines.stream();
         }
